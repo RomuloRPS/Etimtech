53474092b3ff9dc955f25a69f64d0daea0848024
RomuloRPS <romulo.rps@hotmail.com>
2019-10-06 10:07:51 -0300

    cart modal

---
+++ src/app/cart-modal/cart-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-cart-modal',
+  templateUrl: './cart-modal.component.html',
+  styleUrls: ['./cart-modal.component.css']
+})
+export class CartModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
